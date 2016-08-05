ef3c2668bffbe5610f1ba039b94954095b5c995d
weak_ptr <weak_ptr@163.com>
2016-08-05 21:57:48 +0800

    create softlink

---
+++ doinst.sh
@@ -0,0 +1,2 @@
+(rm /usr/bin/sublime_text; ln -s /opt/sublime_text/sublime_text /usr/bin/sublime_text)
+(rm /usr/share/applications/sublime_text.desktop; ln -s /opt/sublime_text/sublime_text.desktop /usr/share/applications/sublime_text.desktop)
